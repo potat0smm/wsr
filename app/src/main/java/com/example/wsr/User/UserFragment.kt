f486c5c54d84f85906ecdbad07dc447df0196af4
wsr <stepan.morozov.18@inbox.ru>
2023-04-14 17:54:25 +0500

    apiCamFront

--- app/src/main/java/com/example/wsr/User/UserFragment.kt
+++ app/src/main/java/com/example/wsr/User/UserFragment.kt
@@ -1,8 +1,11 @@
 package com.example.wsr.User
 
+import android.Manifest
 import android.annotation.SuppressLint
 import android.app.Activity
+import android.app.Instrumentation.ActivityResult
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.graphics.Bitmap
 import android.graphics.BitmapFactory
 import android.net.Uri
@@ -10,13 +13,17 @@ import android.os.Bundle
 import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
-import android.view.RoundedCorner
 import android.view.View
 import android.view.ViewGroup
 import android.widget.AdapterView
 import android.widget.ArrayAdapter
 import android.widget.AutoCompleteTextView
-import androidx.navigation.fragment.findNavController
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContract
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
 import com.bumptech.glide.request.RequestOptions
@@ -32,7 +39,7 @@ import java.io.InputStream
 class UserFragment : Fragment() {
 
     private val REQUEST_IMAGE_CAPTURE = 1
-
+    private lateinit var pLauncher: ActivityResultLauncher<String>
 
     private var _binding: FragmentUserBinding? = null
     private val binding get() = _binding!!
@@ -63,11 +70,34 @@ class UserFragment : Fragment() {
         }
 
         binding.imageUser.setOnClickListener {
-            openGallery()
+            if(ContextCompat.checkSelfPermission(requireContext(),Manifest.permission.CAMERA) == PackageManager.PERMISSION_GRANTED){
+                checkCamera()
+            }else{
+                requestCameraPermission()
+            }
         }
-
-
         return binding.root
+    }
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            if (grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                // Разрешение на использование камеры было предоставлено, открываем камеру
+                checkCamera()
+            } else {
+                // Разрешение на использование камеры было отклонено, показываем сообщение
+                Toast.makeText(requireContext(), "Permission denied", Toast.LENGTH_SHORT).show()
+            }
+        }
+    }
+    private fun requestCameraPermission() {
+        if (shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)) {
+            // Если пользователь уже отклонил разрешение, но еще не выбрал "Не спрашивать снова",
+            // показываем ему объяснение необходимости разрешения
+            Toast.makeText(requireContext(), "Need your permission to access the camera", Toast.LENGTH_SHORT).show()
+        } else {
+            // Запрашиваем разрешение
+            ActivityCompat.requestPermissions(requireActivity(), arrayOf(Manifest.permission.CAMERA), REQUEST_IMAGE_CAPTURE)
+        }
     }
 
     private fun openGallery() {
@@ -82,17 +112,31 @@ class UserFragment : Fragment() {
             try {
                 val inputStream: InputStream? = imageUri?.let { context?.contentResolver?.openInputStream(it) }
                 val bitmap: Bitmap = BitmapFactory.decodeStream(inputStream)
-               // binding.imageUser.setImageBitmap(bitmap)
+                // binding.imageUser.setImageBitmap(bitmap)
 
                 Glide.with(requireActivity())
                     .load(bitmap)
                     .apply(RequestOptions().transform(RoundedCorners(500)))
-                    .override(150,150)
                     .into(binding.imageUser)
 
 
             } catch (e: FileNotFoundException) {
                 e.printStackTrace()
+            }
+        }
+    }
+    private fun checkCamera(){
+        when{
+            ContextCompat.checkSelfPermission(requireContext(),Manifest.permission.CAMERA)
+                    == PackageManager.PERMISSION_GRANTED ->{
+                openGallery()
+                Toast.makeText(requireContext(),"Camera run",Toast.LENGTH_SHORT).show()
+            }
+            shouldShowRequestPermissionRationale(Manifest.permission.CAMERA)->{
+                Toast.makeText(requireContext(),"Need yout permisson",Toast.LENGTH_SHORT).show()
+            }
+            else->{
+
             }
         }
     }
